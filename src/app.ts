8f271bbbc3ff5e126ec39d1c110c5c7b609692ba
JorgeMarles <jamarlesf@gmail.com>
2025-10-28 09:26:56 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+
+import { RegisterRoutes } from "./generated/routes.js";
+import * as middlewares from "./middlewares.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"));
+
+RegisterRoutes(app);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  }),
+);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
---
+++ src/env.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+  PORT: z.coerce.number().default(3000),
+  DIVISIST_URL: z.string().url("Given DIVISIST_URL isn't a valid URL"),
+  DELAY_MS: z.number().int("Delat should be an integer representing the miliseconds").default(5000),
+});
+
+try {
+  // eslint-disable-next-line node/no-process-env
+  envSchema.parse(process.env);
+}
+catch (error) {
+  if (error instanceof z.ZodError) {
+    console.error("Error with Environment Variable:", error.message);
+  }
+  else {
+    console.error(error);
+  }
+  process.exit(1);
+}
+
+// eslint-disable-next-line node/no-process-env
+export const env = envSchema.parse(process.env);
